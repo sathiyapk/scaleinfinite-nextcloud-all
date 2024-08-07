c04861967a6411fc01d181116f0f839e650a2088
John J <graphicssource@gmail.com>
2024-08-07 14:52:00 +0530

    Created custom theme and disabled unwanted things

--- apps/settings/templates/settings/personal/_old-personal.info.php
+++ apps/settings/templates/settings/personal/_old-personal.info.php
@@ -27,64 +27,54 @@ script('settings', [
 
 <div id="personal-settings">
 	<h2 class="hidden-visually"><?php p($l->t('Personal info')); ?></h2>
-		<?php if ($_['profileEnabledGlobally']) : ?>
-		<!-- <div class="personal-settings-setting-box personal-settings-setting-box-profile">
+	<div id="vue-avatar-section"></div>
+	<?php if ($_['profileEnabledGlobally']) : ?>
+		<div class="personal-settings-setting-box personal-settings-setting-box-profile">
 			<div id="vue-profile-section"></div>
-		</div> -->
-		<!-- cloudfloat123!@# This whole file disalbled for personal info  -->
-		<!-- <div class="personal-settings-setting-box personal-settings-setting-box-detail">
+		</div>
+		<div class="personal-settings-setting-box personal-settings-setting-box-detail">
 			<div id="vue-details-section"></div>
-		</div> -->
+		</div>
 	<?php else: ?>
-		<!-- <div class="personal-settings-setting-box personal-settings-setting-box-detail--without-profile">
+		<div class="personal-settings-setting-box personal-settings-setting-box-detail--without-profile">
 			<div id="vue-details-section"></div>
-		</div> -->
+		</div>
 	<?php endif; ?>
 	<div class="personal-settings-setting-box">
 		<div id="vue-displayname-section"></div>
-		<div id="vue-phone-section"></div>
+	</div>
+	<div class="personal-settings-setting-box">
 		<div id="vue-email-section"></div>
 	</div>
-	
-	<!-- <div class="personal-settings-setting-box">
-		<div id="vue-email-section"></div>
-	</div> -->
-	
-	<!-- <div class="personal-settings-setting-box">
+	<div class="personal-settings-setting-box">
 		<div id="vue-phone-section"></div>
-	</div> -->
+	</div>
 	<div class="personal-settings-setting-box">
 		<div id="vue-location-section"></div>
-		<div id="vue-birthday-section"></div>
 	</div>
 	<div class="personal-settings-setting-box">
-	<div id="vue-avatar-section"></div>
-	</div>
-	<!-- <div class="personal-settings-setting-box">
 		<div id="vue-birthday-section"></div>
-	</div> -->
-	
-	<!-- <div class="personal-settings-setting-box personal-settings-language-box">
+	</div>
+	<div class="personal-settings-setting-box personal-settings-language-box">
 		<div id="vue-language-section"></div>
-	</div> -->
-	<!-- cloudfloat123!@# This whole file disalbled for personal info  -->
-	<!-- <div class="personal-settings-setting-box personal-settings-locale-box">
+	</div>
+	<div class="personal-settings-setting-box personal-settings-locale-box">
 		<div id="vue-locale-section"></div>
-	</div> -->
-	<!-- <div class="personal-settings-setting-box">
+	</div>
+	<div class="personal-settings-setting-box">
 		<div id="vue-fdow-section"></div>
-	</div> -->
-	<!-- <div class="personal-settings-setting-box">
+	</div>
+	<div class="personal-settings-setting-box">
 		<div id="vue-website-section"></div>
-	</div> -->
-	<!-- <div class="personal-settings-setting-box">
+	</div>
+	<div class="personal-settings-setting-box">
 		<div id="vue-twitter-section"></div>
-	</div> -->
-	<!-- <div class="personal-settings-setting-box">
+	</div>
+	<div class="personal-settings-setting-box">
 		<div id="vue-fediverse-section"></div>
-	</div> -->
-	<?php// if ($_['profileEnabledGlobally']) : ?>
-		<!-- <div class="personal-settings-setting-box">
+	</div>
+	<?php if ($_['profileEnabledGlobally']) : ?>
+		<div class="personal-settings-setting-box">
 			<div id="vue-organisation-section"></div>
 		</div>
 		<div class="personal-settings-setting-box">
@@ -95,15 +85,14 @@ script('settings', [
 		</div>
 		<div class="personal-settings-setting-box">
 			<div id="vue-biography-section"></div>
-		</div> -->
-	<!-- <?php //endif; ?> -->
-	<!-- <span class="msg"></span> -->
+		</div>
+	<?php endif; ?>
+	<span class="msg"></span>
 
-	<!-- <div id="personal-settings-group-container"></div> -->
+	<div id="personal-settings-group-container"></div>
 </div>
-<!-- Dissabbled profile visability -->
-<!-- <?php // if ($_['profileEnabledGlobally']) : ?>
+<?php if ($_['profileEnabledGlobally']) : ?>
 	<div class="personal-settings-section">
 		<div id="vue-profile-visibility-section"></div>
 	</div>
-<?php //endif; ?> -->
+<?php endif; ?>
