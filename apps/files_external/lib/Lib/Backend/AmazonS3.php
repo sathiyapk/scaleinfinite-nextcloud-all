5c7ce7b93253bf3db389434003a14b717071c405
John J <graphicssource@gmail.com>
2025-10-13 13:18:24 +0530

    AmazonS3.php uploaded in apps/files_external/lib/Lib/Backend/AmazonS3.php

--- apps/files_external/lib/Lib/Backend/AmazonS3.php
+++ apps/files_external/lib/Lib/Backend/AmazonS3.php
@@ -4,7 +4,7 @@
  * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-namespace OCA\Files_External\Lib\Backend;
+namespace OCA\Files_External\Lib\Backend; 
 
 use OCA\Files_External\Lib\Auth\AmazonS3\AccessKey;
 use OCA\Files_External\Lib\Auth\AuthMechanism;
